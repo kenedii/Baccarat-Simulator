c278388d88cfd5bbee62ccce578f10be914a109f
kenedii <55466055+kenedii@users.noreply.github.com>
2024-02-21 20:26:20 -0500

    Add files via upload
    -Fixed logic in PBPPBB and subtractifLose functions.
    -fixed payouts and money deduction
    -fixed player balances going into negative
    - added case for players < 2 in MultiplePlayerSimulation function. it caused an error when returning stdev with 1 player. 
    - fixed how the player object defined its starting balance and bet. 
         - now returns [average, median, stdev, variance, balances] for 2 or more players, [average, median, variance, balances] for one player

--- Martingale-PBPPBB.py
+++ Martingale-PBPPBB.py
@@ -5,9 +5,10 @@ import statistics
 # Simulation of the Baccarat strategy seen in this video: https://www.youtube.com/watch?v=g1JpoE2UyF8
 
 class Player():  # A player. Can play rounds of the martingale strategy
-    def __init__(self, starting_money=100, starting_bet=1):
-        starting_bet = 1
-        starting_money = 100
+    class Player():
+        def __init__(self, starting_money=100, starting_bet=1):
+            self.starting_bet = starting_bet
+            self.starting_money = starting_money
 
 
 def cancelIfNegative(money):  # Stops gambling if player is broke
@@ -23,15 +24,15 @@ def subtractifLose(player, current_bet, win_on, game):  # The logic to subtract 
         player.starting_money = player.starting_money + 9 * current_bet
         return [player, 1]
     elif game[0] == win_on and game[0] == 'Player':  # Player winning
-        player.starting_money = player.starting_money + 2 * current_bet
+        player.starting_money = player.starting_money + (2 * current_bet)
         return [player, 1]
     elif game[0] == win_on and game[0] == 'Banker':  # Banker winning
-        player.starting_money = player.starting_money + 1.95 * current_bet
+        player.starting_money = player.starting_money + (1.95 * current_bet)
         return [player, 1]
     elif (game[0] != 'Tie') and (game[0] != win_on):  # Player losing
-        player.starting_money = player.starting_money - current_bet
         return [player, 0]
     else:  # Player and banker tie and tie was not bet on
+        player.starting_money = player.starting_money + current_bet
         return [player, 2]
 
 
@@ -43,28 +44,35 @@ def dontBetOverMoney(current_bet, money):  # Dont place a bet that will send pla
 
 
 def PBPPBB(player):  # The logic for the strategy. Bets on PBPPBB doubling up each loss.
-    current_bet = player.starting_bet
+    current_bet = dontBetOverMoney(player.starting_bet, player.starting_money)
+    if current_bet > player.starting_money:
+        current_bet = player.starting_money  # Adjust the bet to match available money
+    player.starting_money -= current_bet
+
     if cancelIfNegative(player.starting_money) == 0:
         return [player.starting_money, player.starting_bet]
 
+    while True:
+        game_result = subtractifLose(player, current_bet, 'Player', bg.game())  # Fetch the entire result
+        player = game_result[0]
+        if game_result[1] == 1:
+            return [player.starting_money, player.starting_bet]
+        elif game_result[1] == 2:  # If it's a tie and the player didn't bet on it
+            continue
+        else:
+            current_bet *= 2
+            if cancelIfNegative(player.starting_money) == 0:
+                return [player.starting_money, player.starting_bet]
+            current_bet = dontBetOverMoney(current_bet, player.starting_money)
+            break
+
     current_bet = dontBetOverMoney(current_bet, player.starting_money)
-    player.starting_money -= current_bet
-    while True:
-        game_result = subtractifLose(player, current_bet, 'Player', bg.game())  # Fetch the entire result
-        if game_result[1] == 1:
-            return [player.starting_money, player.starting_bet]
-        elif game_result[1] == 2:  # If it's a tie and the player didn't bet on it
-            continue
-        else:
-            current_bet *= 2
-            if cancelIfNegative(player.starting_money) == 0:
-                return [player.starting_money, player.starting_bet]
-            current_bet = dontBetOverMoney(current_bet, player.starting_money)
-            break
-
+    if current_bet > player.starting_money:
+        current_bet = player.starting_money  # Adjust the bet to match available money
     player.starting_money -= current_bet
     while True:
         game_result = subtractifLose(player, current_bet, 'Banker', bg.game())  # Fetch the entire result
+        player = game_result[0]
         if game_result[1] == 1:
             return [player.starting_money, player.starting_bet]
         elif game_result[1] == 2:  # If it's a tie and the player didn't bet on it
@@ -76,9 +84,13 @@ def PBPPBB(player):  # The logic for the strategy. Bets on PBPPBB doubling up ea
             current_bet = dontBetOverMoney(current_bet, player.starting_money)
             break
 
+    current_bet = dontBetOverMoney(current_bet, player.starting_money)
+    if current_bet > player.starting_money:
+        current_bet = player.starting_money  # Adjust the bet to match available money
     player.starting_money -= current_bet
     while True:
         game_result = subtractifLose(player, current_bet, 'Player', bg.game())  # Fetch the entire result
+        player = game_result[0]
         if game_result[1] == 1:
             return [player.starting_money, player.starting_bet]
         elif game_result[1] == 2:  # If it's a tie and the player didn't bet on it
@@ -90,9 +102,13 @@ def PBPPBB(player):  # The logic for the strategy. Bets on PBPPBB doubling up ea
             current_bet = dontBetOverMoney(current_bet, player.starting_money)
             break
 
+    current_bet = dontBetOverMoney(current_bet, player.starting_money)
+    if current_bet > player.starting_money:
+        current_bet = player.starting_money  # Adjust the bet to match available money
     player.starting_money -= current_bet
     while True:
         game_result = subtractifLose(player, current_bet, 'Player', bg.game())  # Fetch the entire result
+        player = game_result[0]
         if game_result[1] == 1:
             return [player.starting_money, player.starting_bet]
         elif game_result[1] == 2:  # If it's a tie and the player didn't bet on it
@@ -104,9 +120,13 @@ def PBPPBB(player):  # The logic for the strategy. Bets on PBPPBB doubling up ea
             current_bet = dontBetOverMoney(current_bet, player.starting_money)
             break
 
+    current_bet = dontBetOverMoney(current_bet, player.starting_money)
+    if current_bet > player.starting_money:
+        current_bet = player.starting_money  # Adjust the bet to match available money
     player.starting_money -= current_bet
     while True:
         game_result = subtractifLose(player, current_bet, 'Banker', bg.game())  # Fetch the entire result
+        player = game_result[0]
         if game_result[1] == 1:
             return [player.starting_money, player.starting_bet]
         elif game_result[1] == 2:  # If it's a tie and the player didn't bet on it
@@ -118,9 +138,13 @@ def PBPPBB(player):  # The logic for the strategy. Bets on PBPPBB doubling up ea
             current_bet = dontBetOverMoney(current_bet, player.starting_money)
             break
 
+    current_bet = dontBetOverMoney(current_bet, player.starting_money)
+    if current_bet > player.starting_money:
+        current_bet = player.starting_money  # Adjust the bet to match available money
     player.starting_money -= current_bet
     while True:
         game_result = subtractifLose(player, current_bet, 'Banker', bg.game())  # Fetch the entire result
+        player = game_result[0]
         if game_result[1] == 1:
             return [player.starting_money, player.starting_bet]
         elif game_result[1] == 2:  # If it's a tie and the player didn't bet on it
@@ -157,19 +181,23 @@ def MultiplePlayerSimulation(players, startingmoney, startingbet, time):
     average = sum(balances) / len(balances)  # Mean balance out of every player
     median = statistics.median(balances)
     stdev = statistics.stdev(balances)
-    variance = statistics.variance(balances,average)
-    return [average, median, stdev, variance, balances]
+    variance = statistics.variance(balances)
+
+    if players >= 2:
+        return [average, median, stdev, variance, balances]
+    else: # Cannot calculate the standard deviation with 1 player.
+        return [average, median, variance, balances]
 
 
 def __main__():
-    players = 20000
+    players = 200
     startingmoney = 10000
-    startingbet = 20
-    time = 200
+    startingbet = 15
+    time = 25
     results = MultiplePlayerSimulation(players, startingmoney, startingbet, time)
-    print(results[4]) # Print the balance of every player at the end of the simulation
+    print(results[4])  # Print the balance of every player at the end of the simulation
     print('Average Player Money:', results[0], 'Median player money', results[1])
-    print('Standard deviation:', results[2] + '  Variance:', results[3])
+    print('Standard deviation:', results[2], '  Variance:', results[3])
 
 
 __main__()
